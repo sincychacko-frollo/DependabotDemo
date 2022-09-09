8cef8dc29c53b7de9b6770f6a1f0797809eb2092
Sincy Chacko <sincy.chacko@frollo.us>
2022-09-09 11:26:16 +1000

    Initial Commit

---
+++ Shared/DependabotDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DependabotDemoApp.swift
+//  Shared
+//
+//  Created by Sincy Chacko on 9/9/2022.
+//
+
+import SwiftUI
+
+@main
+struct DependabotDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
